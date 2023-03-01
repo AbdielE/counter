f0595e93fe3d52638e64154548f9ab45fb603a49
Abdiel Esteban Alfonso Chavez <isc19350558@gmail.com>
2023-03-01 11:06:55 -0600

    Botones para decrementar e inicializar en 0

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,40 @@ function App() {
   //let counter = 0;
   const [counter, setCounter] = useState(0)
 
-  return(
+  return (
     <div>
       <h3>Counter</h3>
-      <hr/>
+      <hr />
       <h1>{counter}</h1>
       <button
-        onClick={ () =>{
+        onClick={() => {
           setCounter(counter + 1);
           console.log(counter)
         }
         }
         type="button"
-        className="btn btn-primary">
-          +1
+        className="btn btn-success">
+        +1
+      </button>
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+          console.log(counter)
+        }
+        }
+        type="button"
+        className="btn btn-danger">
+        -1
+      </button>
+      <button
+        onClick={() => {
+          setCounter(0);
+          console.log(counter)
+        }
+        }
+        type="button"
+        className="btn btn-secondary">
+        0
       </button>
     </div>
   )
